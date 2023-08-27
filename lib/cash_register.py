4716fbd3ae10902ab9978529af2d64480f771f67
Vitamax <vitamaxoduo@gmail.com>
2023-08-28 02:43:50 +0300

    Completed

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction = None
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title] * quantity)
+        self.last_transaction = price * quantity
+
+    def apply_discount(self):
+        if self.discount:
+            self.total -= (self.discount / 100) * self.total
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.last_transaction:
+            self.total -= self.last_transaction
+            self.last_transaction = None
